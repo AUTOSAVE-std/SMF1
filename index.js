60144b6fdcfe5cd7735337f788826bd086c28a46
หมี โหด <eemachine@msn.com>
2015-08-23 23:58:01 +0700

    init
    1

---
+++ index.js
@@ -0,0 +1,43 @@
+var express = require('express'); 
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.use(express.static(__dirname + '/public'));
+
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    console.log('message: ' + msg);
+  });
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
+  socket.on('image', function(msg){
+    io.emit('image', msg);
+     console.log('image sent');
+  });
+
+
+  socket.on('bypass1', function(msg){
+    console.log('bypass1: ' + msg);
+  });
+  socket.on('bypass1', function(msg){
+    io.emit('bypass1', msg);
+  });
+
+
+  console.log('a user connected');
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
